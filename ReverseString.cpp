da227261a49ab491df3d0497928953c7009dfca8
Taaji <rindu23@gmail.com>
2016-12-14 12:56:30 -0500

    Update ReverseString.cpp

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -1,39 +1,20 @@
-clude<stdio.h>
-
+#include<stdio.h>
 #include<string.h>
-
 #include<stdlib.h>
 
-
-
 char* reverseString(char* s) {
-
     int k;                  //for computing the length of the input string.
-
     int i;                  //counter
-
     int c = 0;              //for storing in new array
-
     
-
     char* d;                //new array
-
     
-
     k = strlen(s);
-
     strcpy(d, s);
 
-
-
 	for(i = k - 1; i >= 0; i--){
-
 		d[c] = s[i];
-
 		c++;
-
 	}
-
 	return d;
-
 }
